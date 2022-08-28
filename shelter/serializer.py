72c509b1dc4d588d7c57b0de805459cb56fdf2ef
Marni Hager <mlh6118@gmail.com>
2022-08-28 12:34:23 -0700

    Fixed ForeignKey issue.

--- shelter/serializer.py
+++ shelter/serializer.py
@@ -3,7 +3,10 @@ from .models import Animal
 
 
 class AnimalSerializer(serializers.ModelSerializer):
+    cared_for_by = serializers.CharField(source='cared_for_by.username')
+    carer_email = serializers.CharField(source='cared_for_by.email')
+
     class Meta:
         fields = ('animal_id', 'animal_name', 'animal_type', 'breed',
-                  'personality', 'cared_by', 'created_at', 'updated_at')
+                  'personality', 'cared_for_by', 'carer_email')
         model = Animal
